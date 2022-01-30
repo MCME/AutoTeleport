655e069cde646c756b65141f272aa2fafff62f92
EriolEandur <Eriol_Eandur@gmx.de>
2022-01-30 22:16:01 +0100

    fixed 1.18 conversion script

--- 1.18 Fix/Autoteleport_fix.py
+++ 1.18 Fix/Autoteleport_fix.py
@@ -14,7 +14,8 @@ with open("PluginData.yml", 'r') as stream:
             data_loaded[name]["locationY"] = data_loaded[name]["locationY"] - 64
             data_loaded[name]["minCornerY"] = data_loaded[name]["minCornerY"] - 64
             data_loaded[name]["maxCornerY"] = data_loaded[name]["maxCornerY"] - 64
-        if data_loaded[name]["target"]["world"] == "world":
-            data_loaded[name]["target"]["y"] = data_loaded[name]["target"]["y"] - 64
+        if "target" in data_loaded[name]:
+            if data_loaded[name]["target"]["world"] == "world":
+                data_loaded[name]["target"]["y"] = data_loaded[name]["target"]["y"] - 64
 with open("PluginData.yml", 'w') as yml_file:
     yml_file.write(yaml.dump(data_loaded, sort_keys=False))
